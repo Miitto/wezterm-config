7fed152811467344bda1440ec4d7bc9165b49a06
Miitto <67914241+Miitto@users.noreply.github.com>
2025-06-30 22:57:38 +0100

    feat: Add WezTerm session manager

--- wezterm.lua
+++ wezterm.lua
@@ -82,4 +82,21 @@ config.window_padding = {
 config.xcursor_theme = "Bibata-Modern-Ice"
 config.xcursor_size = 24
 
+
+local session_manager = require("wezterm-session-manager/session-manager")
+wezterm.on("save_session", function(window) session_manager.save_state(window) end)
+wezterm.on("load_session", function(window) session_manager.load_state(window) end)
+wezterm.on("restore_session", function(window) session_manager.restore_state(window) end)
+
+config.leader = { key = 'z', mods = 'CTRL', timeout_milliseconds = 1000}
+
+
+config.keys = {
+  {key = "S", mods = "LEADER", action = wezterm.action{EmitEvent = "save_session"}},
+  {key = "L", mods = "LEADER", action = wezterm.action{EmitEvent = "load_session"}},
+  {key = "R", mods = "LEADER", action = wezterm.action{EmitEvent = "restore_session"}},
+  -- Allow CTRL-Z passthrough by pressing CTRL-Z, CTRL-Z
+  {key = 'z', mods = 'LEADER|CTRL', action = wezterm.action.SendKey { key = 'z', mods = 'CTRL' },},
+}
+
 return config
